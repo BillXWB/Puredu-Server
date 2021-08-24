85d6709084da547e3f10b4bf307004fa2ea26213
Kirin <1370302499@qq.com>
2021-08-24 18:10:26 +0800

    🐛Fix(sql): 删除 database.sql 中的不兼容语句

--- src/main/sql/database.sql
+++ src/main/sql/database.sql
@@ -6,8 +6,6 @@ CREATE DATABASE puredu
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Chinese (Simplified)_China.936'
-    LC_CTYPE = 'Chinese (Simplified)_China.936'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
