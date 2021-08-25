dae0839e7ee23f054b9289e8cfc8bd70e0e86ac3
Kirin <1370302499@qq.com>
2021-08-25 16:07:34 +0800

    ✨Feat(opedukg): 实现选择题的预处理

--- src/main/java/edu/pure/server/opedukg/entity/OpedukgExercise.java
+++ src/main/java/edu/pure/server/opedukg/entity/OpedukgExercise.java
@@ -1,12 +1,34 @@
 package edu.pure.server.opedukg.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class OpedukgExercise {
     private final int id;
     private final String question;
-    private final String answer;
+    private final List<String> options;
+    private final int answer;
+
+    public OpedukgExercise(final int id,
+                           final @NotNull String question,
+                           final @NotNull String answer) {
+        final Logger logger = LoggerFactory.getLogger(OpedukgExercise.class);
+        // TODO 非选择题
+        this.id = id;
+        final List<String> segments = Arrays.stream(question.split("[A-Z][．.]")) // 可能是全角句点...
+                                            .map(String::strip)
+                                            .filter(Predicate.not(String::isEmpty))
+                                            .collect(Collectors.toList());
+        this.question = segments.get(0);
+        this.options = segments.subList(1, segments.size());
+        this.answer = answer.toCharArray()[0] - 'A';
+    }
 }
